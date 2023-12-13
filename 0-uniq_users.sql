2b78708d1481414c29890580c2929e3eb99509d0
Joseph Otine <joeotine@gmail.com>
2023-12-13 11:36:12 -0800

    All tasks completed

--- 0-uniq_users.sql
+++ 0-uniq_users.sql
@@ -1,8 +1,7 @@
--- Write a SQL script that creates a table users
-
-CREATE TABLE IF NOT EXISTS users (
-    id int NOT NULL AUTO_INCREMENT,
+-- Creates a table with unique users.
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    PRIMARY KEY (id)
+    name VARCHAR(255)
 );
